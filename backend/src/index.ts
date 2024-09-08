c8ea651c0fc829cc2821399d73a12551b3769a2e
Gero <gtutos3@gmail.com>
2024-09-07 21:19:33 -0500

    Backend - Conexion a la base de datos

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
+// Importacion de express
 import express from 'express';
 
+// Creacion de la aplicacion express
 const app = express();
 
+// Definicion del puerto
 const PORT = 3000;
 
+// ejecucion de la aplicacion
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
