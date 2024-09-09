0916c93603d9d8efebb63c13bc9980059f080a2d
Gero <gtutos3@gmail.com>
2024-09-08 21:43:50 -0500

    Backend - Creacion de middleware de seguridad de JWt

--- backend/src/libs/jwt.ts
+++ backend/src/libs/jwt.ts
@@ -1,12 +1,19 @@
-import jwt from 'jsonwebtoken';
+// Importacion de jsonwebtoken
+import jwt from "jsonwebtoken";
 
-const SECRET_KEY: string = "thisisasecretkeyforjsonwebtoken";
+// Clave secreta para firmar el token
+export const SECRET_KEY: string = "thisisasecretkeyforjsonwebtoken";
 
+// Funcion para crear un token de acceso
 export function createAccessToken(payload: any) {
+  // Retornamos una promesa con el token de acceso
   return new Promise((resolve, reject) => {
-    jwt.sign(payload, SECRET_KEY, { expiresIn: '1d' }, (err, token) => {
+    // Firmamos el token con el payload y la clave secreta
+    jwt.sign(payload, SECRET_KEY, { expiresIn: "1d" }, (err, token) => {
+      // Si ocurre un error, rechazamos la promesa
       if (err) reject(err);
+      // Retornamos el token
       resolve(token);
     });
-  })
+  });
 }
