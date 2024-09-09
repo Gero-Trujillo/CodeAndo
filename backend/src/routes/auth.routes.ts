85302acaa964bbfe2d9d1e6bcf242f91c765e260
Gero <gtutos3@gmail.com>
2024-09-08 21:49:40 -0500

    Backend - Creacion de ruta de logout en authRoutes

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,7 +1,7 @@
 // Importacion de Router de express
 import {Router} from 'express';
 // Importacion de las funciones de registro y login
-import {register, login} from '../controllers/auth.controller';
+import {register, login, logout} from '../controllers/auth.controller';
 
 // Instanciacion de Router
 const router = Router();
@@ -9,6 +9,7 @@ const router = Router();
 // Rutas de registro y login
 router.post('/register', register);
 router.post('/login', login);
+router.post('/logout', logout);
 
 // Exportacion de las rutas
 export default router;
