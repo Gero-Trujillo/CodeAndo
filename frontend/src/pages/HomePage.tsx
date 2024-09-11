019fe9113de9f815687258d7955c28dba34a0aea
Gero <gtutos3@gmail.com>
2024-09-11 14:54:05 -0500

    Frontend - HomePage responsive, se agregaron unos estilos en layout

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import NavMobile from '../components/NavMobile'
 import CreatePost from '../components/CreatePost'
 import PostComponent from '../components/PostComponent'
+import NavPc from '../components/NavPc'
 
 function HomePage() {
   return (
     <>
-      <section className='w-full'>
+      <section className='w-full lg:hidden'>
         <NavMobile/>
         <section className='p-2 flex flex-col gap-4'>
             <CreatePost/>
@@ -15,6 +16,22 @@ function HomePage() {
             <PostComponent/>
             <PostComponent/>
         </section>
+      </section>
+
+      <section className="hidden w-full h-screen p-8 lg:flex">
+        <div className="w-1/4">
+          <NavPc/>
+        </div>
+        <div className='flex flex-col flex-1 px-20 xl:px-40 2xl:px-60 gap-4 overflow-y-auto scrollbar-hide'>
+          <CreatePost/>
+          <PostComponent/>
+          <PostComponent/>
+          <PostComponent/>
+          <PostComponent/>
+          <PostComponent/>
+          <PostComponent/>
+          <PostComponent/>
+        </div>
       </section>
     </>
   )
