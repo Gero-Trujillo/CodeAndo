9f6f97088d7a21e95df7ffa7ebe80cbccc791b25
Gero <gtutos3@gmail.com>
2024-09-11 12:33:39 -0500

    Frontend - Creacion de AuthPage

---
+++ frontend/src/pages/AuthPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AuthComponent from '../components/AuthComponent'
+
+function AuthPage() {
+  return (
+    <>
+      <AuthComponent />
+    </>
+  )
+}
+
+export default AuthPage
